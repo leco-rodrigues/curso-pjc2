390ff5f3ae7178ea565f9b9be18a82b84ddcfcaa
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-01-27 13:11:50 -0300

    Aula 2
    Resolução do exercício da Aula 2.

--- nivel1/media.py
+++ nivel1/media.py
@@ -11,3 +11,21 @@ print(media_simples)
 print(media_harmonica)
 print(media_ponderada)
 # -------------------- [Exercício] Aula 2
+
+
+# média simples
+# n1 = 10
+# n2 = 4
+# print("Os números são: ", n1, "e", n2)
+# ms = (n1 + n2) / 2
+# print('Media simples:', ms)
+# harmônica
+# mh = 2 / (1/n1 + 1/n2)
+# print("media harmônica: ", mh)
+# ponderada
+# p1 = 2
+# p2 = 4
+# print("Pesos são:", p1, "e", p2)
+# mp = (p1 * n1 + p2 * n2) / (p1 + p2)
+# print("Médias ponderada: ", mp)
+# ------------------------------- (Resolução) [Exercício] Aula 2
