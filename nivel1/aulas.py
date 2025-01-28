2c784ce4ba2fdbc974a3b956e01fc15290e4eafe
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-01-28 09:34:51 -0300

    Update aulas.py
    Aula 5 adicionada

--- nivel1/aulas.py
+++ nivel1/aulas.py
@@ -100,7 +100,7 @@
 
 
 # "io.py"
-# palvra = input("Digite uma palvra: ")
+# palavra = input("Digite uma palvra: ")
 # print(palavra)
 #
 # nota = input("Digite uma nota: ")
@@ -112,3 +112,25 @@
 # else:
 #    print(f'Reprovado com a nota {nota} (média é {media})')
 # ---------------------------------------------------------- Aula 4
+
+
+# "agregados.py"
+# nota_matematica = 7.8
+# nota_portugues = 8.2
+# nota_filosofia = 9.5
+#
+# nomes = ("Maria", "João", "Paula")
+# print(nomes[0], nomes[-1])
+# nomes[1] = "Marcelo"
+#
+# notas = [7.8, 8.2, 9.5]
+# disciplinas = ["matemática", "português", "filosofia"]
+# disciplinas.append("história")
+# notas[0] = notas[0] + 1
+# notas.append(9)
+# print(notas, type(notas), type(notas[0]))
+# print(f"A nota de {disciplinas[0]} foi {notas[0]})
+# print(f"A nota de {disciplinas[-2]} foi {notas[-2]})
+# print(disciplinas[3])
+# print(notas[3])
+# --------------- Aula 5
