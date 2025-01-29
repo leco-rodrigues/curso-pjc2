60802333fadb7ca36551d5c8a07b94eedb34fe30
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-01-29 10:48:34 -0300

    Update aulas.py

--- nivel1/aulas.py
+++ nivel1/aulas.py
@@ -2,10 +2,6 @@
  DO CANAL Programação Dinâmica!!!"""
 
 
-"""!!!CONTEÚDO COPIADO DIRETAMENTE DOS VÍDEOS DO CURSO Python do Jeito Certo 2.0,
- DO CANAL Programação Dinâmica!!!"""
-
-
 # "ola.py"
 # print("Olá Mundo")
 # ------------------ Aula 0
