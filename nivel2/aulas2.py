7b1028f1d3efca54a7f210dd2d071418f80202bf
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-01-29 20:55:04 -0300

    Create aulas2.py
    Aula 10 adicionada.

---
+++ nivel2/aulas2.py
@@ -0,0 +1,30 @@
+"""!!!CONTEÚDO COPIADO DIRETAMENTE DOS VÍDEOS DO CURSO Python do Jeito Certo 2.0,
+ DO CANAL Programação Dinâmica!!!"""
+
+
+# "funcoes.py"
+# num = int(input("Digite um número: "))
+# num = num + 3
+# print(num)
+#
+# def multiplicar_por_7(n):
+#     res = 7 * n
+#     return res
+#
+# def media_simples(a, b):
+#     return (a + b) / 2
+#
+# r = multiplicar_por_7(10)
+# print("Resultado é", r)
+# print(media_simples(10, 8))
+# res = multiplicar_por_7(6)
+# print(res)
+# a = 8
+# b = 32
+# print(media_simples(b, a))
+#
+# def pedir_inscricao():
+#     print("Inscreva-se no canal Programação Dinâmica")
+#
+# pedir_inscricao()
+# ----------------- Aula 10
