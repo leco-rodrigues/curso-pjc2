a10bafb6c688c4a9fc496ee97599fda1ff28db80
Alex Rodrigues <alex2199rodrigues@gmail.com>
2025-01-29 22:45:47 -0300

    Create formar_triangulo.py
    Exercício da Aula 10.

---
+++ nivel2/formar_triangulo.py
@@ -0,0 +1,30 @@
+# Criar uma função, com três parâmetros, que diga se é possível formar um triângulo
+
+# Passo 1: Criar a função 
+def formar_triangulo(a, b, c):
+    if a + b > c and a + c > b and b + c > a:
+        return True
+    return False
+
+# Passo 2: Receber os argumentos
+    # Função para otimizar o processo
+def lado_triangulo(a = 'Insira um valor para um dos lados do triângulo: '):
+    while True:
+        try:
+            lado = int(input(a).strip())
+            if lado > 0:
+                return lado
+            print('Por favor, insira um valor positivo.')
+        except ValueError:
+            print('Por favor, insira um número inteiro positivo.')
+
+a = lado_triangulo('Insira um valor para o primeiro lado do triângulo: ')
+b = lado_triangulo('Insira um valor para o segundo lado do triângulo: ')
+c = lado_triangulo('Insira um valor para o terceiro lado do triângulo: ')
+
+# Passo 3: Chamar a função e exibir o resultado
+if formar_triangulo(a, b, c):
+    print('É possível formar um triângulo!')
+else:
+    print('Não é possível formar um triângulo...')
+# ------------------------------------------------ [Exercício] Aula 10
